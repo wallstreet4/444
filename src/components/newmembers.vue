c782949a9c25692f1308929bfd0811ff0169b74d
Baby-China <17633288602@163.com>
2020-05-03 15:06:40 +0800

    aa

---
+++ src/components/newmembers.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="footer">
+		<h1>
+			底部区域
+		</h1>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang='stylus'>
+	.footer{
+		h1{
+			color: green;
+		}
+	}
+</style>
