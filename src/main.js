4de761c3a47278feba5964092fefef7b206665ea
AnLiang <799341465@qq.com>
2020-05-04 18:11:47 +0800

    18:11

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
 
+
+import 'swiper/dist/css/swiper.css';
+
+
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
- 
+
 //组件模块
 import index from '@/components/index'
+import latest from '@/components/latest'
 
- 
+
 Vue.use(Router)
- 
+
 export default new Router({
   routes: [
     { path: '/', name: 'index', component: index },
-    
+    { path: '/latest', name: 'latest', component: latest }
+
   ]
 })
