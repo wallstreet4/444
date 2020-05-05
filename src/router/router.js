4051b33d6e7d0e1f6ae38b5966e4490f0849a6a9
Baby-China <17633288602@163.com>
2020-05-05 18:09:52 +0800

    5-5 18:09

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
- 
 //组件模块
 import index from '@/components/index'
+import newmembers from "@/components/newmembers"
+import memberlogin from "@/components/memberlogin"
+import contactus from "@/components/contactus"
 
- 
 Vue.use(Router)
- 
+
 export default new Router({
-  routes: [
-    { path: '/', name: 'index', component: index },
-    
-  ]
+	routes: [{
+			path: '/',
+			name: 'index',
+			component: index
+		},
+		{
+			path: "/newmembers",
+			name: "newmembers",
+			component: newmembers
+		},
+		{
+			path: "/memberlogin",
+			name: "memberlogin",
+			component: memberlogin
+		},
+		{
+			path: "/contactus",
+			name: "contactus",
+			component: contactus
+		}
+	]
 })
