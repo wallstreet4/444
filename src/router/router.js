8ae7b9edbc2ef2dfc39742c5cecca3772aebc45f
AnLiang <799341465@qq.com>
2020-05-05 17:30:04 +0800

    17:29

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 //组件模块
 import index from '@/components/index'
 import latest from '@/components/latest'
+import wallstreet from '@/components/wallstreet'
 
 
 Vue.use(Router)
@@ -11,7 +12,8 @@ Vue.use(Router)
 export default new Router({
   routes: [
     { path: '/', name: 'index', component: index },
-    { path: '/latest', name: 'latest', component: latest }
+    { path: '/latest', name: 'latest', component: latest },
+    { path: '/wallstreet', name: 'wallstreet', component: wallstreet }
 
   ]
 })
