85b80d7b7381901db7c4fda424200cdc1686699c
JRuiGG <r_6969@163.com>
2020-05-09 12:34:27 +0800

    zjr occ is ok

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router/router.js'
 
 Vue.config.productionTip = false
+import vueSwiper from 'vue-awesome-swiper'
+import '../node_modules/swiper/css/swiper.css'
+
+Vue.use(vueSwiper)
 
 new Vue({
   render: h => h(App),
